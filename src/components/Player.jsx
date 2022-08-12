1b715a35bd80b465bd7da2a93e0c021930f445ba
MdTasib <ohidulalam442@gmail.com>
2022-08-12 22:16:45 +0600

    9.Update Time

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { faAngleLeft, faAngleRight, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Player({currentSong: {audio}, isPlaying, setIsPlaying}) {
+    const [songInfo, setSongInfo] = useState({
+        duration: null,
+        currentTime: null,
+    });
     const audioRef = useRef(null);
 
+    //  GET PLAYER TIME
+    const playTimeHandler = e => {
+        const duration = e.target.duration;
+        const currentTime = e.target.currentTime;
+        setSongInfo({...songInfo, duration, currentTime});
+    };
+
+    // TIME FORMAT FUNCTION
+    const getTime = time => {
+        return Math.floor(time/60) + ":" + ("0" + Math.floor(time%60)).slice(-2);
+    }
+
+    // TOGGLE PLAY BUTTON (PLAY / PAUSE);
     const playSongHandler = () => {
         if(isPlaying){
             audioRef.current.pause();
@@ -18,16 +35,16 @@ function Player({currentSong: {audio}, isPlaying, setIsPlaying}) {
     return (
         <div className='player'>
             <div className="time-control">
-                <p>start time</p>
+                <p>{getTime(songInfo.currentTime)}</p>
                 <input type="range" />
-                <p>end time</p>
+                <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft} />
                 <FontAwesomeIcon onClick={playSongHandler} className='play' size='2x' icon={faPlay} />
                 <FontAwesomeIcon className='skib-forward' size='2x' icon={faAngleRight} />
             </div>
-            <audio ref={audioRef} src={audio}></audio>
+            <audio onTimeUpdate={playTimeHandler} onLoadedMetadata={playTimeHandler} ref={audioRef} src={audio}></audio>
         </div>
     );
 }
