b0907ce5a870bc011a3096fd9df9405f0212063d
MdTasib <ohidulalam442@gmail.com>
2022-08-12 18:17:16 +0600

    initial styleing

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Song() {
     return (
         <div className='song-container'>
-            <h2>Song</h2>
+            <h2>Picture</h2>
+            <h2>name</h2>
+            <h2>Artice</h2>
         </div>
     );
 }
