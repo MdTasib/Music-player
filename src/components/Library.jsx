17f87317d751d38ff863cc72e432b043be94b397
MdTasib <ohidulalam442@gmail.com>
2022-08-13 10:51:44 +0600

    14.Selecting Songs

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import LibrarySong from './LibrarySong';
 
-function Library({songs}) {
+function Library({songs, setCurrentSong}) {
     return (
         <div className='library'>
             <h2>Library</h2>
             <div className="library-songs">
-                {songs.map(song => <LibrarySong key={song.id} song={song}/>)}
+                {songs.map(song => <LibrarySong setCurrentSong={setCurrentSong} id={song.id}  songs={songs} key={song.id} song={song}/>)}
             </div>
         </div>
     );
--- src/components/LibrarySong.jsx
+++ src/components/LibrarySong.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-function LibrarySong({song: {name, artist, cover}}) {
+function LibrarySong({song: {name, artist, cover}, setCurrentSong, songs, id}) {
+    const songSelectHandler = () => {
+        const selectedSong = songs.filter(state => state.id === id);
+        setCurrentSong(selectedSong[0]);
+    }
+
     return (
-        <div className='library-song'>
+        <div onClick={songSelectHandler} className='library-song'>
             <img src={cover} alt={name} />
             <div className="song-description">
                 <h3>{name}</h3>
